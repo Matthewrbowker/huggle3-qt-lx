407ce2f0f989c1d3ad37f8780ed892ddb2b236a4
Matthew Bowker <matthewrbowker@me.com>
2016-05-23 18:02:01 -0600

    Re-enable qt5 and attempt to fix (I expect this to break...)

--- travis/install.sh
+++ travis/install.sh
@@ -6,6 +6,9 @@ fi
 
 if [ "$QTTYPE" = "5" ]; then
 	sudo apt-get install -y ubuntu-sdk qtquick1-5-dev cmake
+	sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+    sudo apt-get update -qq
+    sudo apt-get install -qq qt5-qmake qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev
 fi
 
 sudo apt-get install g++-4.7
